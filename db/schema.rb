aab5279f340278f999eba947e759709247a66b3d
Jordan Herrera <jordanherrerajrh@gmail.com>
2017-08-25 10:39:54 -0700

    Added comment scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825022229) do
+ActiveRecord::Schema.define(version: 20170825173636) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
